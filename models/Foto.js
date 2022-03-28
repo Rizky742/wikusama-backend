d986fbb559800492d202661fd2e1dfb52ddfab44
cowdingus <zaki.nadhif45@gmail.com>
2022-03-28 19:21:01 +0700

    Rewrite Foto model and its relation

--- models/Foto.js
+++ models/Foto.js
@@ -1,24 +1,44 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    const Foto = sequelize.define('Foto', {
-        id: {
-            type: DataTypes.INTEGER(11),
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false
-          },
-          path: {
-            type: DataTypes.STRING(255)
-          },
-          deskripsi: {
-            type: DataTypes.TEXT
-          },
-          alamat: {
-            type: DataTypes.STRING(255)
-          },
-    }, {
-        tableName: 'Foto',
-        timestamps: false
-    })
-
-    return Foto;
-}
+  class Foto extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Foto.belongsTo(models.User, {
+        foreignKey: "owner_id"
+      })
+    }
+  }
+  Foto.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    owner_id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+    },
+    path: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    deskripsi: {
+      type: DataTypes.TEXT,
+    },
+    alamat: {
+      type: DataTypes.STRING(255)
+    },
+  }, {
+    sequelize,
+    modelName: 'Foto',
+  });
+  return Foto;
+};
