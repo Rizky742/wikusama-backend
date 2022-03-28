acc41195e2e49811b7d6cd4f20567008ff8364d1
cowdingus <zaki.nadhif45@gmail.com>
2022-03-28 23:33:40 +0700

    Add `judul` field into Foto and rewrite `Foto` model

--- models/Foto.js
+++ models/Foto.js
@@ -1,21 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Foto extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      Foto.belongsTo(models.User, {
-        foreignKey: "owner_id"
-      })
-    }
-  }
-  Foto.init({
+  const Foto = sequelize.define('Foto', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
@@ -30,6 +14,9 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING(255),
       allowNull: false,
     },
+    judul: {
+      type: DataTypes.STRING(255),
+    },
     deskripsi: {
       type: DataTypes.TEXT,
     },
@@ -37,8 +24,15 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING(255)
     },
   }, {
-    sequelize,
-    modelName: 'Foto',
+    tableName: 'Foto',
+    timestamps: false
   });
+
+  Foto.associate = (models) => {
+    Foto.belongsTo(models.User, {
+      foreignKey: "owner_id"
+    });
+  };
+
   return Foto;
 };
