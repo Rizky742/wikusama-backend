c0060476fa3a5db21787e7f1388a3f981187f70d
cowdingus <zaki.nadhif45@gmail.com>
2022-05-07 15:52:19 +0700

    Add wildcard cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const register = require('./routes/register');
@@ -16,6 +17,7 @@ const deleteFotoProfile = require('./routes/delete-foto-profil')
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
