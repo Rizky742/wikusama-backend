8387472f92d188379eae7a0809ac2b8672d16810
Rizky742 <rizkyardiansyah967@gmail.com>
2022-05-11 13:54:42 +0700

    add get kota and province

--- routes/alamat.js
+++ routes/alamat.js
@@ -5,6 +5,20 @@ const region = require('./region/regions.json')
 const { Provinsi } = require('../models')
 const { Kota } = require('../models')
 
+app.get('/kota', async(req,res) => {
+    Kota.findAll()
+    .then(result => {
+        res.json(result)
+    })
+})
+
+app.get('/provinsi', async(req,res) => {
+    Provinsi.findAll()
+    .then(result => {
+        res.json(result)
+    })
+})
+
 app.post('/', async(req,res) => {
 
     region.map((result,index) => {
